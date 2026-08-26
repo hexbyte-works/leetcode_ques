e4cbad0b45f5a7861778c617e6fb6dc01fcf816f
Peteti Sai Ruthvik <peteti.ruthvik@gmail.com>
2026-08-26 23:48:32 +0530

    Time: 23 ms (92.70%) | Memory: 82.9 MB (80.26%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for(int i= 0;i<nums.size();i++){
-            mp[nums[i]]++;
-            if(mp[nums[i]]>1){
+        sort(nums.begin(),nums.end());
+        for(int i= 0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]){
                 return true;
             }
         }
