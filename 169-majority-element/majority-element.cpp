2fb1e60c89baae076af50731e1b707d10c5e7398
Peteti Sai Ruthvik <peteti.ruthvik@gmail.com>
2026-08-29 00:02:03 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (75.65%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int, int> mp;
-
-        for (int i = 0; i < nums.size(); i++) {
-            mp[nums[i]]++;
-
-            if (mp[nums[i]] > nums.size() / 2) {
+        map <int,int> mp;
+        for(int i =0; i<nums.size(); i++){
+            mp[nums[i]]+=1;
+            if(mp[nums[i]]>(nums.size()/2)){
                 return nums[i];
             }
         }
-
-        return -1;
+         return -1;
     }
 };
