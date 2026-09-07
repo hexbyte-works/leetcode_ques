287635ca13a081ee731354cb79486ee1b0a7a2ce
Peteti Sai Ruthvik <peteti.ruthvik@gmail.com>
2026-09-08 00:01:35 +0530

    Time: 75 ms (27.33%) | Memory: 47.7 MB (26.83%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int count=0,total=0;
+        map <int,int> mp;
+        for(int i : nums){
+            total+=i;
+            if(total==k){
+                count+=1;
+            }
+            if(mp.find(total-k)!=mp.end()){
+                count+=mp[total-k];
+            }
+            mp[total]+=1;
+        }
+        return count;
+    }
+};
